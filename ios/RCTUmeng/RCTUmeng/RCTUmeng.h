e5a727c2decbd76af47cbfd4b6badc94ea9866e3
messense <messense@icloud.com>
2017-05-12 16:53:17 +0800

    Remove duplicated onEvent method in iOS native module

--- ios/RCTUmeng/RCTUmeng/RCTUmeng.h
+++ ios/RCTUmeng/RCTUmeng/RCTUmeng.h
@@ -16,5 +16,5 @@
 @interface RCTUmeng : NSObject <RCTBridgeModule>
 - (void) startWithAppkey:(NSString *) appkey;
 - (void) setDebugMode:(BOOL)value;
-- (void) onEvent:(NSString *)eventId;
+- (void) onEvent:(NSString *)eventId attributes:(NSDictionary *)attributes counter:(NSString *)counter;
 @end
